101fdbdf19e418f492f3612a654146c5ae264479
alan_curtindoafesta <alan_curtindoafesta@192.168.0.102>
2014-04-12 23:29:19 -0300

    Enviando email de participante correto

--- src/com/bean/ParticipanteBean.java
+++ src/com/bean/ParticipanteBean.java
@@ -25,7 +25,7 @@ public class ParticipanteBean {
 		this.participante = participante;
 	}
 	
-	public void confirma()
+	public void validaCampos()
 	{
 
 
@@ -44,11 +44,11 @@ public class ParticipanteBean {
 		email.setSmtpPort(465);
 		email.setSSLOnConnect(true);
 		email.addTo(p.getEmail(), p.getNome()); 
-		email.setFrom("rakemanage@gmail.com", "Me"); //Senha rake2014
+		email.setFrom("rakemanage@gmail.com", "eCongress"); //Senha rake2014
 		email.setAuthentication("rakemanage@gmail.com", "rake2014");
-		email.setSubject("Confirmação de Inscricao"); 
-		email.setMsg(" Parabens,"+p.getNome()+ "!\n Sua inscricao foi realizada com sucesso.\n Numero de inscricao: "+ inscricao+" \n Congresso "+p.getCongresso());   
-		//email.attach(attachment); 
+		email.setSubject(p.getCongresso()+ " - Confirmacao de Inscricao"); 
+		email.setMsg(" Ola, "+p.getNome()+ ".\n Sua inscricao foi realizada com sucesso!\n Numero de inscricao: "
+		+ inscricao+" \n Congresso: "+p.getCongresso());   
 		
 		// adiciona o anexo   
 		email.send();// envia o e-mail
@@ -61,7 +61,7 @@ public class ParticipanteBean {
 	
 	public String grava() throws EmailException
 	{ 		
-		confirma();
+		validaCampos();
 		
 		System.out.println("Gravando Participante");
 		new DAO<Participante>(Participante.class).adiciona(this.participante);
